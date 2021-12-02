8a24ba92d397968376f9dd58b14a40f277fc76bc
Jeremy Vissault <vissault@et.esiea.fr>
2021-12-02 15:24:41 +0100

    Partie 4

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
@@ -0,0 +1,29 @@
+package fr.lernejo.tester.internal;
+
+import fr.lernejo.tester.SomeLernejoTests;
+import fr.lernejo.tester.api.TestMethod;
+import java.util.List;
+import java.lang.reflect.Method;
+
+public class TestClassDescriptionLernejoTests
+{
+    @TestMethod
+    public void test()
+    {
+        final TestClassDescription testClassDescription = new TestClassDescription(SomeLernejoTests.class);
+        final List<Method> methodes = testClassDescription.listTestMethods();
+
+        if(methodes.stream().noneMatch(method -> method.getName().equals("ok")))
+        {
+            throw new AssertionError("Doit avoir une méthode test ok()");
+        }
+        if(methodes.stream().noneMatch(method -> method.getName().equals("ko")))
+        {
+            throw new AssertionError("Doit avoir une méthode test ko()");
+        }
+        if (methodes.size() != 2)
+        {
+            throw new AssertionError("Doit avoir 2 classes méthodes");
+        }
+    }
+}
